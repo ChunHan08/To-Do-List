0e60279670a701a5ca183159a03f1d908292f15f
ChunHan08 <149086722+ChunHan08@users.noreply.github.com>
2024-08-09 15:13:33 +0000

    todo list

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const inputField = document.querySelector(".input-field textarea"),
 todoLists = document.querySelector(".todoLists"),
-pendingNum = document.querySelector(".pendingNum"),
+pendingNum = document.querySelector(".pending-num"),
 clearButton = document.querySelector(".clear-button");
+function allTasks() {
+  let tasks = document.querySelectorAll(".pending");
+  pendingNum.textContent = tasks.length === 0 ? "no" : tasks.length;
+}
 inputField.addEventListener("keyup", (e) => {
   let inputVal = inputField.value.trim();
   console.log(inputVal);
   if (e.key === "Enter" && inputVal.length > 0) {
-    let liTag = ` <li class="list">
+    let liTag = ` <li class="list pending" onclick="handleStatus(this)">
           <input type="checkbox" />
-          <span class="task">Remember to be sigma.</span>
-          <i class="uil uil-trash"></i>
+          <span class="task">${inputVal}</span>
+          <i class="uil uil-trash" onclick="deleteTask(this)"></i>
         </li>`;
-        todoLists.insertAdjacentElement("beforeend", liTag);
+        todoLists.insertAdjacentHTML("beforeend", liTag);
+        inputField.value = "";
+        allTasks();
   }
 });
+function handleStatus(e) {
+  const checkbox = e.querySelector("input");
+  checkbox.checked = checkbox.checked ? false : true;
+  e.classList.toggle("pending");
+  allTasks();
+}
+function deleteTask() {
+  e.parentElement.remove();
+}
+clearButton.addEventListener("click", () => {
+  todoLists.innerHTML = "";
+  allTasks()
+})
